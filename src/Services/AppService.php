f3d752ce275f54ba8f877867691c963950d23537
Matt Lantz <mattlantz@gmail.com>
2016-07-22 22:41:25 -0400

    Minor fix

--- src/Services/AppService.php
+++ src/Services/AppService.php
@@ -9,16 +9,12 @@ class AppService
 
     public function getAppNamespace()
     {
-        if (! is_null($this->namespace)) {
-            return $this->namespace;
-        }
-
         $composer = json_decode(file_get_contents(base_path('composer.json')), true);
 
         foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path) {
             foreach ((array) $path as $pathChoice) {
                 if (realpath(app()->path()) == realpath(base_path().'/'.$pathChoice)) {
-                    return $this->namespace = $namespace;
+                    return $namespace;
                 }
             }
         }
