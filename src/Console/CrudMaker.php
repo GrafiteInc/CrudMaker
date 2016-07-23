4a7da5ed1db1b166dc634384e77113d9ba8bdedc
Matt Lantz <mattlantz@gmail.com>
2016-07-23 00:12:33 -0400

    Further improvements

--- src/Console/CrudMaker.php
+++ src/Console/CrudMaker.php
@@ -5,6 +5,8 @@ namespace Yab\CrudMaker\Console;
 use Config;
 use Exception;
 use Illuminate\Console\Command;
+use Yab\CrudMaker\Generators\ConfigTrait;
+use Yab\CrudMaker\Generators\GeneratorTrait;
 use Yab\CrudMaker\Generators\CrudGenerator;
 use Yab\CrudMaker\Generators\DatabaseGenerator;
 use Yab\CrudMaker\Services\AppService;
@@ -12,6 +14,9 @@ use Yab\CrudMaker\Services\ValidatorService;
 
 class CrudMaker extends Command
 {
+    use ConfigTrait;
+    use GeneratorTrait;
+
     /**
      * Column Types.
      *
@@ -72,20 +77,35 @@ class CrudMaker extends Command
     protected $description = 'Generate a magical CRUD for a table with options for: Migration, API, UI, Schema and even Relationships';
 
     /**
+     * The app service
+     *
+     * @var AppService
+     */
+    protected $appService;
+
+    /**
+     * The validator service
+     *
+     * @var ValidatorService
+     */
+    protected $validator;
+
+    /**
      * Generate a CRUD stack.
      *
      * @return mixed
      */
     public function handle()
     {
-        $validator = new ValidatorService();
-        $appService = new AppService();
+        $this->appService = new AppService();
+        $this->validator = new ValidatorService();
+
         $section = '';
         $splitTable = [];
 
         $appPath = app()->path();
         $basePath = app()->basePath();
-        $appNamespace = $appService->getAppNamespace();
+        $appNamespace = $this->appService->getAppNamespace();
         $framework = ucfirst('Laravel');
 
         if (stristr(get_class(app()), 'Lumen')) {
@@ -142,7 +162,7 @@ class CrudMaker extends Command
 
         $config['schema'] = $this->option('schema');
         $config['relationships'] = $this->option('relationships');
-        $config['template_source'] = $this->getTemplates($framework, $basePath);
+        $config['template_source'] = $this->getTemplateConfig($framework, $basePath);
 
         if (stristr($table, '_')) {
             $splitTable = explode('_', $table);
@@ -206,208 +226,6 @@ class CrudMaker extends Command
     }
 
     /**
-     * Set the config of the CRUD.
-     *
-     * @param array  $config
-     * @param string $section
-     * @param string $table
-     * @param array  $splitTable
-     *
-     * @return array
-     */
-    public function configASectionedCRUD($config, $section, $table, $splitTable)
-    {
-        $appPath = app()->path();
-        $basePath = app()->basePath();
-        $appNamespace = $appService->getAppNamespace();
-
-        $sectionalConfig = [
-            '_sectionPrefix_'            => strtolower($section).'.',
-            '_sectionTablePrefix_'       => strtolower($section).'_',
-            '_sectionRoutePrefix_'       => strtolower($section).'/',
-            '_sectionNamespace_'         => ucfirst($section).'\\',
-            '_path_facade_'              => $appPath.'Facades',
-            '_path_service_'             => $appPath.'Services',
-            '_path_repository_'          => $appPath.'Repositories/'.ucfirst($section).'/'.ucfirst($table),
-            '_path_model_'               => $appPath.'Repositories/'.ucfirst($section).'/'.ucfirst($table),
-            '_path_controller_'          => $appPath.'Http/Controllers/'.ucfirst($section).'/',
-            '_path_api_controller_'      => $appPath.'Http/Controllers/Api/'.ucfirst($section).'/',
-            '_path_views_'               => $basePath.'/resources/views/'.strtolower($section),
-            '_path_tests_'               => $basePath.'/tests',
-            '_path_request_'             => $appPath.'Http/Requests/'.ucfirst($section),
-            '_path_routes_'              => $appPath.'Http/routes.php',
-            '_path_api_routes_'          => $appPath.'Http/api-routes.php',
-            '_path_migrations_'          => $basePath.'/database/migrations',
-            'routes_prefix'              => "\n\nRoute::group(['namespace' => '".ucfirst($section)."', 'prefix' => '".strtolower($section)."', 'middleware' => ['web']], function () { \n",
-            'routes_suffix'              => "\n});",
-            '_app_namespace_'            => $appNamespace,
-            '_namespace_services_'       => $appNamespace.'Services\\'.ucfirst($section),
-            '_namespace_facade_'         => $appNamespace.'Facades',
-            '_namespace_repository_'     => $appNamespace.'Repositories\\'.ucfirst($section).'\\'.ucfirst($table),
-            '_namespace_model_'          => $appNamespace.'Repositories\\'.ucfirst($section).'\\'.ucfirst($table),
-            '_namespace_controller_'     => $appNamespace.'Http\Controllers\\'.ucfirst($section),
-            '_namespace_api_controller_' => $appNamespace.'Http\Controllers\Api\\'.ucfirst($section),
-            '_namespace_request_'        => $appNamespace.'Http\Requests\\'.ucfirst($section),
-            '_lower_case_'               => strtolower($splitTable[1]),
-            '_lower_casePlural_'         => str_plural(strtolower($splitTable[1])),
-            '_camel_case_'               => ucfirst(camel_case($splitTable[1])),
-            '_camel_casePlural_'         => str_plural(camel_case($splitTable[1])),
-            '_ucCamel_casePlural_'       => ucfirst(str_plural(camel_case($splitTable[1]))),
-            '_table_name_'               => str_plural(strtolower(implode('_', $splitTable))),
-        ];
-
-        $config = array_merge($config, $sectionalConfig);
-        $config = array_merge($config, app('config')->get('crudmaker.sectioned', []));
-        $config = $this->setConfig($config, $section, $table);
-
-        $pathsToMake = [
-            '_path_repository_',
-            '_path_model_',
-            '_path_controller_',
-            '_path_api_controller_',
-            '_path_views_',
-            '_path_request_',
-        ];
-
-        foreach ($config as $key => $value) {
-            if (in_array($key, $pathsToMake) && !file_exists($value)) {
-                mkdir($value, 0777, true);
-            }
-        }
-
-        return $config;
-    }
-
-    /**
-     * Get the templates directory
-     *
-     * @param  string $framework
-     * @param  string $basePath
-     *
-     * @return string
-     */
-    public function getTemplates($framework, $basePath)
-    {
-        $templates = __DIR__.'/../Templates/'.$framework;
-
-        if ($framework === 'Laravel') {
-            $templateDirectory = $basePath.'/resources/crudmaker/crud';
-            if (is_dir($templateDirectory)) {
-                $templates = app('config')->get('crudmaker.template_source', $templateDirectory);
-            }
-        }
-
-        return $templates;
-    }
-
-    /**
-     * Set the config.
-     *
-     * @param array  $config
-     * @param string $section
-     * @param string $table
-     *
-     * @return array
-     */
-    public function setConfig($config, $section, $table)
-    {
-        if (!empty($section)) {
-            foreach ($config as $key => $value) {
-                $config[$key] = str_replace('_table_', ucfirst($table), str_replace('_section_', ucfirst($section), str_replace('_sectionLowerCase_', strtolower($section), $value)));
-            }
-        } else {
-            foreach ($config as $key => $value) {
-                $config[$key] = str_replace('_table_', ucfirst($table), $value);
-            }
-        }
-
-        return $config;
-    }
-
-    /**
-     * Generate core elements.
-     *
-     * @param \Yab\CrudMaker\Generators\CrudGenerator        $crudGenerator
-     * @param array                                         $config
-     * @param \Symfony\Component\Console\Helper\ProgressBar $bar
-     *
-     * @return void
-     */
-    private function generateCore($crudGenerator, $config, $bar)
-    {
-        $crudGenerator->createRepository($config);
-        $crudGenerator->createService($config);
-
-        if ($config['framework'] === 'laravel') {
-            $crudGenerator->createRequest($config);
-        }
-
-        $bar->advance();
-    }
-
-    /**
-     * Generate app based elements.
-     *
-     * @param \Yab\CrudMaker\Generators\CrudGenerator        $crudGenerator
-     * @param array                                         $config
-     * @param \Symfony\Component\Console\Helper\ProgressBar $bar
-     *
-     * @return void
-     */
-    private function generateAppBased($crudGenerator, $config, $bar)
-    {
-        if (!$this->option('serviceOnly') && !$this->option('apiOnly')) {
-            $crudGenerator->createController($config);
-            $crudGenerator->createViews($config);
-            $crudGenerator->createRoutes($config);
-
-            if ($this->option('withFacade')) {
-                $crudGenerator->createFacade($config);
-            }
-        }
-        $bar->advance();
-    }
-
-    /**
-     * Generate db elements.
-     *
-     * @param \Yab\CrudMaker\Generators\DatabaseGenerator    $dbGenerator
-     * @param \Symfony\Component\Console\Helper\ProgressBar $bar
-     * @param string                                        $section
-     * @param string                                        $table
-     * @param array                                         $splitTable
-     *
-     * @return void
-     */
-    private function generateDB($dbGenerator, $config, $bar, $section, $table, $splitTable)
-    {
-        if ($this->option('migration')) {
-            $dbGenerator->createMigration($config, $section, $table, $splitTable, $this);
-            if ($this->option('schema')) {
-                $dbGenerator->createSchema($config, $section, $table, $splitTable, $this->option('schema'));
-            }
-        }
-        $bar->advance();
-    }
-
-    /**
-     * Generate api elements.
-     *
-     * @param \Yab\CrudMaker\Generators\CrudGenerator        $crudGenerator
-     * @param array                                         $config
-     * @param \Symfony\Component\Console\Helper\ProgressBar $bar
-     *
-     * @return void
-     */
-    private function generateAPI($crudGenerator, $config, $bar)
-    {
-        if ($this->option('api') || $this->option('apiOnly')) {
-            $crudGenerator->createApi($config);
-        }
-        $bar->advance();
-    }
-
-    /**
      * Generate a CRUD report.
      *
      * @param string $table
@@ -447,6 +265,6 @@ class CrudMaker extends Command
             }
         } else {
             $this->info("\nYou will want to create a migration in order to get the $table tests to work correctly.\n");
-        }
+    }
     }
 }
---
+++ src/Generators/ConfigTrait.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Yab\CrudMaker\Generators;
+
+trait ConfigTrait
+{
+    /**
+     * Set the config of the CRUD.
+     *
+     * @param array  $config
+     * @param string $section
+     * @param string $table
+     * @param array  $splitTable
+     *
+     * @return array
+     */
+    public function configASectionedCRUD($config, $section, $table, $splitTable)
+    {
+        $appPath = app()->path();
+        $basePath = app()->basePath();
+        $appNamespace = $this->appService->getAppNamespace();
+
+        $sectionalConfig = [
+            '_sectionPrefix_'            => strtolower($section).'.',
+            '_sectionTablePrefix_'       => strtolower($section).'_',
+            '_sectionRoutePrefix_'       => strtolower($section).'/',
+            '_sectionNamespace_'         => ucfirst($section).'\\',
+            '_path_facade_'              => $appPath.'Facades',
+            '_path_service_'             => $appPath.'Services',
+            '_path_repository_'          => $appPath.'Repositories/'.ucfirst($section).'/'.ucfirst($table),
+            '_path_model_'               => $appPath.'Repositories/'.ucfirst($section).'/'.ucfirst($table),
+            '_path_controller_'          => $appPath.'Http/Controllers/'.ucfirst($section).'/',
+            '_path_api_controller_'      => $appPath.'Http/Controllers/Api/'.ucfirst($section).'/',
+            '_path_views_'               => $basePath.'/resources/views/'.strtolower($section),
+            '_path_tests_'               => $basePath.'/tests',
+            '_path_request_'             => $appPath.'Http/Requests/'.ucfirst($section),
+            '_path_routes_'              => $appPath.'Http/routes.php',
+            '_path_api_routes_'          => $appPath.'Http/api-routes.php',
+            '_path_migrations_'          => $basePath.'/database/migrations',
+            'routes_prefix'              => "\n\nRoute::group(['namespace' => '".ucfirst($section)."', 'prefix' => '".strtolower($section)."', 'middleware' => ['web']], function () { \n",
+            'routes_suffix'              => "\n});",
+            '_app_namespace_'            => $appNamespace,
+            '_namespace_services_'       => $appNamespace.'Services\\'.ucfirst($section),
+            '_namespace_facade_'         => $appNamespace.'Facades',
+            '_namespace_repository_'     => $appNamespace.'Repositories\\'.ucfirst($section).'\\'.ucfirst($table),
+            '_namespace_model_'          => $appNamespace.'Repositories\\'.ucfirst($section).'\\'.ucfirst($table),
+            '_namespace_controller_'     => $appNamespace.'Http\Controllers\\'.ucfirst($section),
+            '_namespace_api_controller_' => $appNamespace.'Http\Controllers\Api\\'.ucfirst($section),
+            '_namespace_request_'        => $appNamespace.'Http\Requests\\'.ucfirst($section),
+            '_lower_case_'               => strtolower($splitTable[1]),
+            '_lower_casePlural_'         => str_plural(strtolower($splitTable[1])),
+            '_camel_case_'               => ucfirst(camel_case($splitTable[1])),
+            '_camel_casePlural_'         => str_plural(camel_case($splitTable[1])),
+            '_ucCamel_casePlural_'       => ucfirst(str_plural(camel_case($splitTable[1]))),
+            '_table_name_'               => str_plural(strtolower(implode('_', $splitTable))),
+        ];
+
+        $config = array_merge($config, $sectionalConfig);
+        $config = array_merge($config, app('config')->get('crudmaker.sectioned', []));
+        $config = $this->setConfig($config, $section, $table);
+
+        $pathsToMake = [
+            '_path_repository_',
+            '_path_model_',
+            '_path_controller_',
+            '_path_api_controller_',
+            '_path_views_',
+            '_path_request_',
+        ];
+
+        foreach ($config as $key => $value) {
+            if (in_array($key, $pathsToMake) && !file_exists($value)) {
+                mkdir($value, 0777, true);
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * Get the templates directory
+     *
+     * @param  string $framework
+     * @param  string $basePath
+     *
+     * @return string
+     */
+    public function getTemplateConfig($framework, $basePath)
+    {
+        $templates = __DIR__.'/../Templates/'.$framework;
+
+        if ($framework === 'Laravel') {
+            $templateDirectory = $basePath.'/resources/crudmaker/crud';
+            if (is_dir($templateDirectory)) {
+                $templates = app('config')->get('crudmaker.template_source', $templateDirectory);
+            }
+        }
+
+        return $templates;
+    }
+
+    /**
+     * Set the config.
+     *
+     * @param array  $config
+     * @param string $section
+     * @param string $table
+     *
+     * @return array
+     */
+    public function setConfig($config, $section, $table)
+    {
+        if (!empty($section)) {
+            foreach ($config as $key => $value) {
+                $config[$key] = str_replace('_table_', ucfirst($table), str_replace('_section_', ucfirst($section), str_replace('_sectionLowerCase_', strtolower($section), $value)));
+            }
+        } else {
+            foreach ($config as $key => $value) {
+                $config[$key] = str_replace('_table_', ucfirst($table), $value);
+            }
+        }
+
+        return $config;
+    }
+}
