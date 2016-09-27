64fd7927b410a39143a3817f523d94254b6530b2
Matt Lantz <mattlantz@gmail.com>
2016-09-27 16:59:30 -0400

    New naming convention

--- src/Generators/CrudGenerator.php
+++ src/Generators/CrudGenerator.php
@@ -44,7 +44,7 @@ class CrudGenerator
             $request = str_replace($key, $value, $request);
         }
 
-        $request = $this->filesystem->put($config['_path_controller_'].'/'.$config['_camel_case_'].'Controller.php', $request);
+        $request = $this->filesystem->put($config['_path_controller_'].'/'.$config['_ucCamel_casePlural_'].'Controller.php', $request);
 
         return $request;
     }
