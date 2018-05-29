742c932aa6d6f84a2a72e1efdb14e8699beaed0e
Matt Lantz <mattlantz@gmail.com>
2018-05-29 08:58:32 -0400

    Case sensitivity fix

--- tests/Generators/ArtisanTest.php
+++ tests/Generators/ArtisanTest.php
@@ -115,7 +115,7 @@ class ArtisanTest extends TestCase
 
     public function testFactory()
     {
-        $file = $this->destinationDir.'/database/factories/BookFactory.php';
+        $file = $this->destinationDir.'/database/factories/bookFactory.php';
         $contents = file_get_contents($file);
 
         $this->assertContains('Book::class', $contents);
